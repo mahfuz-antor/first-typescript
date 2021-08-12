5985fd6fa57252ed0deb0853d4138e0dbac5d48c
mahfuz-antor <antorcnj2014@gmail.com>
2021-08-12 21:10:24 +0600

    new branch mahfuz

--- src/components/AllTasks.tsx
+++ src/components/AllTasks.tsx
@@ -1,40 +1,53 @@
 import React, { useState } from 'react';
 import NewTask from './NewTask';
 
+interface ITask {
+    name: string,
+    email: string,
+}
+
 const AllTasks = () => {
 
     const [task, setTask] = useState("")
-// console.log('All Input here', task)
+    const [taskMail, setEmail] = useState("")
+    console.log('Task Name', task)
+    console.log('Task Mail', taskMail)
     const [taskList, setTaskList] = useState<string[]>([])
 
     const AddButton = () => {
         setTaskList([...taskList, task])
         setTask("")
+        setEmail("")
     }
     console.log('click this', taskList)
 
     return (
         <div style={{
-            backgroundColor:'#b09b62', 
+            backgroundColor: '#b09b62',
             color: 'rgb(3 100 117)',
-            overflow:'hidden'
+            overflow: 'hidden'
         }}>
-            <h1 className="p-5">All Tasks</h1>
+            <h1 className="p-5"> All Tasks</h1>
             <div className="form">
-                <input style={{width: '25%', border: '2px solid blue', borderRadius: '10px', fontSize: '20px'}}
-                onChange={(e) => setTask(e.target.value)}
-                name='name' placeholder="Task Name" type="text" />
-                <button style={{width: '5%', border: '2px solid blue',borderRadius: '10px', fontSize: '20px'}} onClick={AddButton}>Add</button>
+                <input style={{ width: '25%', boxShadow: '0px 5px 20px blue', border: 'none', margin: '5px', borderRadius: '7px', fontSize: '20px' }}
+                    onBlur={(e) => setTask(e.target.value)}
+                    name='name' placeholder="Task Name" type="text" /> <br />
+
+                <input style={{ width: '25%', boxShadow: '0px 5px 20px blue', border: 'none', margin: '5px', borderRadius: '7px', fontSize: '20px' }}
+                    onBlur={(e) => setEmail(e.target.value)}
+                    name='email' placeholder="example@gmail.com" type="text" /> <br />
+
+                <button style={{ width: '25%', boxShadow: '0px 5px 20px blue', border: 'none', margin: '5px', borderRadius: '7px', fontSize: '20px' }} onClick={AddButton}>Add</button>
             </div>
             <div >
-          
 
-            {
-                taskList.map((task) => <NewTask name={task} email="khan@gmail.com"/>)
-            }
-            
+
+                {
+                    taskList.map((task) => <NewTask name={task} email={task} />)
+                }
+
             </div>
-          
+
         </div>
     );
 };
